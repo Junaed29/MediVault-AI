be3e328c6daacb29db3fcb375b766246c7f7f66a
Junaed <junaed1995@gmail.com>
2026-01-28 00:31:08 +0800

    Initial Commit

---
+++ MediVault/MediVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediVaultApp.swift
+//  MediVault
+//
+//  Created by Junaed Chowdhury on 28/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MediVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
